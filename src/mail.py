fe7bcf8abd917b521e2c5461ccd958d8162517c0
Matthew Gleich <matthewgleich@gmail.com>
2019-07-30 15:41:57 -0400

    Create mail.py

---
+++ src/mail.py
@@ -0,0 +1,9 @@
+import smtplib
+
+def send_email(contents, receiver):
+    """
+    Send an email using python
+    :param contents: string that is going to be sent
+    :return: None
+    """
+    

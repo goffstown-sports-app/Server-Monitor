fc9e162b447a8863af7ff511eb35759bf1ae3fed
Matthew Gleich <matthewgleich@gmail.com>
2019-07-31 23:52:46 -0400

    Delete mail.py

--- src/mail.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-def send_email(contents, receiver):
-    """
-    Send an email using python
-    :param contents: string that is going to be sent
-    :return: None
-    """
-    

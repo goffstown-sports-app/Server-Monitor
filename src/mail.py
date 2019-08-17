aa130ffc3caf8aa4b371578371a7b33ed2605fa3
Matthew Gleich <matthewgleich@gmail.com>
2019-08-16 23:14:45 -0400

    Turn email functionality into function

--- src/mail.py
+++ src/mail.py
@@ -1,20 +1,30 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
-import codecs
 
-# msg = MIMEMultipart()
-# msg["From"] = "ghsappbot@gmail.com"
-# msg["To"] = "matthewgleich@gmail.com"
-# with open("email_password.txt") as password_file:
-#     password = password_file.read().strip("\n")
-# msg["Subject"] = "Hello World!"
-# body = "<h1>This works!! Yay!</h1>"
-# msg.attach(MIMEText(body, "html"))
+def send_email(html_content, recipient, subject):
+    """Send an html file to someone
+    
+    Arguments:
+        html_content {string} -- html to send
+        recipient {string} -- who the email will be sent too
+        subject {string} -- what the subject of the email should be
+    """
+    msg = MIMEMultipart() 
+    msg["From"] = "ghsappbot@gmail.com"
+    msg["To"] = recipient
+    with open("email_password.txt") as password_file:
+        password = password_file.read().strip("\n")
+    msg["Subject"] = subject
+    body = html_content
+    msg.attach(MIMEText(body, "html"))
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(msg["From"], password)
+    server.sendmail(msg["From"], msg["To"], msg.as_string())
+    server.quit()
+    
 
-# server = smtplib.SMTP("smtp.gmail.com", 587)
-# server.starttls()
-# server.login(msg["From"], password)
-# server.sendmail(msg["From"], msg["To"], msg.as_string())
-# server.quit()
-
+# Testing
+# send_email('<h1 style="color: red">Problem Detected</h1>\n<p>Looks like there is an error in one of the programs. Below is some information</p>', "matthewgleich@gmail.com", "Testing Testing")
+    

16b35999dfe02d4a7d0c3e69a09d56796dea2a38
Matthew Gleich <matthewgleich@gmail.com>
2019-08-18 21:39:07 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
-from github import Github
+import firebase_admin
+from firebase_admin import db
+from firebase_admin import credentials
+import datetime
+from time import sleep
+from os import system
+
+import database
+import mail
+
 
 def main():
+    """Runs the main program
     """
-    Runs the main file
-    :return: None
-    """
+    cred = credentials.Certificate("firestore_creds.json")
+    firebase_admin.initialize_app(
+        cred, {
+            "databaseURL": "https://ghs-app-5a0ba.firebaseio.com/",
+            'databaseAuthVariableOverride': {
+                'uid': 'my-service-worker'
+            }
+        })
     
-
 
 
 if __name__ == "__main__":
     main()
-

347bf28a4425ef8677a822d88f6078cfe211f387
Matthew Gleich <matthewgleich@gmail.com>
2019-08-18 21:07:31 -0400

    Update pulse

--- src/tests/test-src-database.py
+++ src/tests/test-src-database.py
@@ -29,5 +29,24 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(ref.get(), result)
 
 
+    def test_update_pulse(self):
+        """
+        Test for the update_pulse function
+        """
+        service_name = "Server-Monitor-CI"
+        cred = credentials.Certificate("./../firestore_creds.json")
+        firebase_admin.initialize_app(
+            cred, {
+                "databaseURL": "https://ghs-app-5a0ba.firebaseio.com/",
+                'databaseAuthVariableOverride': {
+                    'uid': 'my-service-worker'
+                }
+            })
+        instance = database.update_pulse(1, service_name)
+        ref = db.reference("db-info/pulses/" + service_name)
+        ref_data = ref.get()
+        self.assertEqual(instance, ref_data)
+
+
 if __name__ == "__main__":
     unittest.main()

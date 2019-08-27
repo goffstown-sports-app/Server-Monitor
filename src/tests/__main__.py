de817bf8d9daedfc922141920b45fb8271205b61
Matthew Gleich <matthewgleich@gmail.com>
2019-08-27 11:31:14 -0400

    Create __main__.py

---
+++ src/tests/__main__.py
@@ -0,0 +1,6 @@
+from os import system
+
+system("python test-src-database.py")
+system("python test-src-mail.py")
+system("python test-utils-datetime_utils.py")
+system("python test-utils-generic.py")

871bf3301bf7a98b75e045f8d3d6fc658ddc2e3e
Matthew Gleich <matthewgleich@gmail.com>
2019-09-17 18:13:48 -0400

    Reformat files for pytest format

---
+++ src/tests/test_src_database.py
@@ -0,0 +1,39 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import pytest
+import sys
+
+sys.path.append("..")
+import database
+
+@pytest.fixture(autouse=True, scope="session")
+def run_around_tests():
+    sys.path.append("..")
+    cred = credentials.Certificate("firestore_creds.json")
+    firebase_admin.initialize_app(
+        cred, {
+            "databaseURL": "https://ghs-app-5a0ba.firebaseio.com/",
+            'databaseAuthVariableOverride': {
+                'uid': 'my-service-worker'
+            }
+        })
+    assert True == True
+
+def test_set_monitoring_info():
+    """Tests for the send email function
+    """
+    diff_time = 4
+    result = database.set_monitoring_info(True, diff_time)
+    ref = db.reference("db-info/monitoring/Server-Monitor")
+    assert ref.get() == result
+
+def test_update_pulse():
+    """
+    Test for the update_pulse function
+    """
+    service_name = "Server-Monitor-CI"
+    instance = database.update_pulse(1, service_name)
+    ref = db.reference("db-info/pulses/" + service_name)
+    ref_data = ref.get()
+    assert instance == ref_data
---
+++ src/tests/test_utils_datetime_utils.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.path.append("..")
+from utils import datetime_utils
+
+
+def test_cast_regular_as_datetime():
+    """
+    Tests the cast_regular_as_datetime function
+    """
+    result = datetime_utils.cast_regular_as_datetime(
+        "2019-08-19 05:51:45.694869")
+    assert result.year == 2019
+    assert result.month == 8
+    assert result.day == 19
+    assert result.hour == 5
+    assert result.minute == 51
+    assert result.second == 45

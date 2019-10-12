828d2ae2cba172776d323f45fb83c1fe8e37df78
Matthew Gleich <matthewgleich@gmail.com>
2019-10-12 12:37:51 -0400

    Delete set_monitoring_info function

--- src/database.py
+++ src/database.py
@@ -4,25 +4,6 @@ import platform
 import os
 
 
-def set_monitoring_info(email_notifications, pulse_time_diff_secs):
-    """Updates the monitoring section for this micro service
-
-    Arguments:
-        email_notifications {bool} -- if the user should get email notifications
-        pulse_time_diff_secs {int} -- amount of seconds between each pulse (exact)
-
-    Returns:
-        dict -- what the monitoring info was set as
-    """
-    ref = db.reference("db-info/monitoring/Server-Monitor")
-    ref_set = {
-        "email-notification": email_notifications,
-        "pulse-time-diffs-(secs)": pulse_time_diff_secs + 300,
-        "pulse-time-diffs-exact-(secs)": pulse_time_diff_secs
-    }
-    ref.set(ref_set)
-    return ref_set
-
 
 def update_pulse(consecutive_number_of_runs, service_name):
     """Updates the pulse for this application

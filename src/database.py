9835bdf56fb74f1f7957923717f0fbd2863e31a4
Matthew Gleich <matthewgleich@gmail.com>
2019-10-12 12:38:02 -0400

    Delete update_pulse function

--- src/database.py
+++ src/database.py
@@ -4,36 +4,6 @@ import platform
 import os
 
 
-
-def update_pulse(consecutive_number_of_runs, service_name):
-    """Updates the pulse for this application
-
-    Arguments:
-        consecutive_number_of_runs {int} -- how many times the application has ran in a row
-        service_name {str} -- name of the service
-
-    Returns:
-        dict -- what the pulse was set as in the database
-    """
-    current_time = str(datetime.now())
-    ref = db.reference("db-info/pulses/" + service_name)
-    if "linux" in str(platform.platform()).lower():
-        ip_command = str(os.popen("hostname -I").read())
-        ip = ip_command.split(" ")[0]
-    else:
-        ip = ""
-    ref_set = {
-        "Pulse-Time": current_time,
-        "Pulse-Amount-(Consecutive)": consecutive_number_of_runs,
-        "Pulse-Node": str(platform.uname().node),
-        "Pulse-OS": str(platform.platform()),
-        "Pulse-Python-Version": str(platform.python_version()),
-        "Pulse-IP": ip
-    }
-    ref.set(ref_set)
-    return ref_set
-
-
 def set_service_status(service_name, status, last_time_online, last_time_offline):
     """Set a true or false status for each service
     
